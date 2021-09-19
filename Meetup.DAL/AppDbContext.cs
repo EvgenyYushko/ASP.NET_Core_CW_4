b56be866189879824c80d4896442ec8f33041e74
evgeny <jeka-krut@mail.ru>
2021-09-19 11:21:41 +0300

    Добавил контекст

--- Meetup.DAL/AppDbContext.cs
+++ Meetup.DAL/AppDbContext.cs
@@ -1,11 +1,17 @@
 ﻿using Microsoft.EntityFrameworkCore;
-using System;
-using System.Collections.Generic;
-using System.Text;
 
 namespace Meetup.DAL
 {
     public class AppDbContext : DbContext
     {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+        {
+            Database.EnsureCreated();
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseLazyLoadingProxies();
+        }
     }
 }
