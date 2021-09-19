8db247c52c417dad71f232d5b4efeb5e67d61e04
evgeny <jeka-krut@mail.ru>
2021-09-19 11:36:28 +0300

    реалзовал репозиторий

---
+++ Meetup.DAL/Patterns/Interfaces/IRepository.cs
@@ -0,0 +1,16 @@
+﻿using Meetup.Models;
+using System;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Meetup.DAL.Patterns.Interfaces
+{
+    public interface IRepository<TEntity> where TEntity : BaseEntity
+    {
+        Task<TEntity> CreateAsync(TEntity item);
+        Task<TEntity> UpdateAsync(TEntity item);
+        Task DeleteAsync(Guid id);
+        IQueryable<TEntity> GetAll();
+        Task<TEntity> GetByIdAsync(Guid id);
+    }
+}
